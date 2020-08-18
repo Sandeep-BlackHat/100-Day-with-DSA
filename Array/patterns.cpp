637109e46293204c6ad85b6de86899a92d272f72
Sandeep Singh <55851630+Sandeep-BlackHat@users.noreply.github.com>
2020-08-18 11:45:11 +0530

    //Reverse Inverted Traingle
    #include<stdio.h>
    
    int main()
    {
    	int i, j, n;
    	printf("Enter the size: "); 
    	scanf("%d",&n);
    	printf("\n");
    	
    	printf("The Inverted Hollow Traingle is: ");
    	printf("\n");
    	for(i=1; i<=n; i++)
    	{	
    		for(j=1; j<= (n * 2 - (2 * i - 1)); j++)
    		{
    			if(i == 1 || j == 1 || j == (n * 2 - (2 * i - 1)))
    			{
    				printf("*");
    			}
    			else
    			{
    				printf(" ");
    			}
    		}
    		printf("\n");
    	}
    	return 0;
    }

--- Array/patterns.cpp
+++ Array/patterns.cpp
@@ -1,1 +1,29 @@
+//Reverse Inverted Traingle
+#include<stdio.h>
 
+int main()
+{
+	int i, j, n;
+	printf("Enter the size: "); 
+	scanf("%d",&n);
+	printf("\n");
+	
+	printf("The Inverted Hollow Traingle is: ");
+	printf("\n");
+	for(i=1; i<=n; i++)
+	{	
+		for(j=1; j<= (n * 2 - (2 * i - 1)); j++)
+		{
+			if(i == 1 || j == 1 || j == (n * 2 - (2 * i - 1)))
+			{
+				printf("*");
+			}
+			else
+			{
+				printf(" ");
+			}
+		}
+		printf("\n");
+	}
+	return 0;
+}
