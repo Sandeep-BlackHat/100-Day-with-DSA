14e10a37f00cac628fbb3d3342cec8923f8589d7
Sandeep Singh <55851630+Sandeep-BlackHat@users.noreply.github.com>
2020-08-18 11:27:13 +0530

    Fibonacci Series
    #include<stdio.h>
    
    int main()
    {
    	int n1 = 0, n2 = 1, n3, i, x;
    	printf("Enter the number upto which Fibonacci series is needed: ");
    	scanf("%d",&x);
    	printf("\n");	
    	printf("The Series is: ");
    	printf("%d %d ",n1,n2);
    	for(i=2; i<x; i++)
    	{
    		n3 = n1 + n2;
    		printf("%d ",n3);
    		n1 = n2;
    		n2 = n3;
    	}
    	
    	return 0;
    }

--- Array/Other_programs/Progm.cpp
+++ Array/Other_programs/Progm.cpp
@@ -109,3 +109,27 @@ int main()
 	
 	return 0;
 }
+
+
+[3] Fibonacci Series
+
+#include<stdio.h>
+
+int main()
+{
+	int n1 = 0, n2 = 1, n3, i, x;
+	printf("Enter the number upto which Fibonacci series is needed: ");
+	scanf("%d",&x);
+	printf("\n");	
+	printf("The Series is: ");
+	printf("%d %d ",n1,n2);
+	for(i=2; i<x; i++)
+	{
+		n3 = n1 + n2;
+		printf("%d ",n3);
+		n1 = n2;
+		n2 = n3;
+	}
+	
+	return 0;
+}
